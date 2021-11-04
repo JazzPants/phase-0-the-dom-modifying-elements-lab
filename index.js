957e65a34e66369f3b67814e3f086e96f7e6b8b2
JazzPants <leighton.l14@gmail.com>
2021-11-04 14:37:33 +1100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+
+/*
+const x = document.getElementById("main");
+x.remove();
+*/
+
+document.getElementById("main").remove(); //if you only need it once in the page
+
+
+const newHeader = document.createElement('h1'); //create h1
+newHeader.setAttribute("id", "victory"); //give it attribute
+newHeader.innerHTML = "Andre is the champion"; //give it content
+
+document.body.append(newHeader); //add to webpage
+
