2cd3ec7790fa25fc9702617f83c34dc27ba6251b
JazzPants <leighton.l14@gmail.com>
2021-11-25 13:51:04 +1100

    Done Second Edit

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const x = document.getElementById("main");
 x.remove();
 */
 
+
+//test #1
 document.getElementById("main").remove(); //if you only need it once in the page
 
 
+//test #2,3,4
 const newHeader = document.createElement('h1'); //create h1
-newHeader.setAttribute("id", "victory"); //give it attribute
-newHeader.innerHTML = "Andre is the champion"; //give it content
+newHeader.setAttribute("id", "victory"); //give it attribute, id="victory"
+newHeader.innerHTML = "Leighton is the champion"; //give it content
 
+//not in test, but make it viewable in browser
 document.body.append(newHeader); //add to webpage
 
